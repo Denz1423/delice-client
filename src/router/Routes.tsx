ed9f3bcdcd81116a7ec2d51011b228b5a19c6a17
Davis <davisdjaja20@gmail.com>
2024-08-22 18:54:43 +1200

    testing hash router

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import App from '@/App';
 import Menu from '@/pages/menu/Menu';
 import Error404 from '@/pages/error/Error404';
@@ -8,7 +8,7 @@ import CheckoutWrapper from '@/pages/checkout/CheckoutWrapper';
 import CheckoutSuccess from '@/pages/checkout/CheckoutSuccess';
 import Order from '@/pages/order/Order';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/',
     element: <App />,
